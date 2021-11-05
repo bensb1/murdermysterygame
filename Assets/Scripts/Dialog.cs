7d11056c0c4a6f00c662ecbc2f2567a4ad1b5d16
bensb1 <162532@chrome.escg.ac.uk>
2021-11-05 14:41:19 +0000

    added in dialogue system now just need to make mutiple options

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    public GameObject option1;
+    public GameObject option2;
+
+     void Start()
+    {
+        StartCoroutine(Type());
+    }
+    IEnumerator Type()
+    {
+        //   yield return new WaitForSeconds(2f);
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+        void Update()
+        {
+            if (textDisplay.text == sentences[index])
+            {
+                continueButton.SetActive(true);
+            }
+        }
+
+    
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if (index < sentences.Length -1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } 
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
