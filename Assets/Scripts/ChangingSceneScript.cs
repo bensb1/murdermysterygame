0cf7e9e31fadb889112e12ddad0e477091589698
bensb1 <56164275+bensb1@users.noreply.github.com>
2021-11-30 12:35:22 +0000

    added in guess the killer scene, won scene, lost scene, made scraf guy beard guy and runner guy scenes link back to the guess the killer scene

--- Assets/Scripts/ChangingSceneScript.cs
+++ Assets/Scripts/ChangingSceneScript.cs
@@ -37,4 +37,24 @@ public class ChangingSceneScript : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void GuessTheKillerToLostScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void GuessTheKillerToWontScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void RunnerGuyToGuessTheKillerScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void BeardGuyToGuessTheKillertScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void ScarfGuyToGuessTheKillertScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
