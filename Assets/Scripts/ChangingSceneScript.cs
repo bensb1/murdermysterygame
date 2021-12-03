755b00229d84a3bbd21bf7174cc7d1297fecfbf0
bensb1 <162532@chrome.escg.ac.uk>
2021-12-03 12:11:36 +0000

    Added in new font for the title on the main menu. Also made scarf guy and scary beard guy navigation images and put new line of code to go to scarf guy scene from runner guy scene.

--- Assets/Scripts/ChangingSceneScript.cs
+++ Assets/Scripts/ChangingSceneScript.cs
@@ -57,4 +57,8 @@ public class ChangingSceneScript : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void RunnerGuyToScrafGuyScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 }
